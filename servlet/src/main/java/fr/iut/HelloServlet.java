75c00fe9796dfe25bd91843d6f11cf5a6779816f
Nogueira Varzea Jorge <jorge.nogueira-varzea@etu.u-bordeaux.fr>
2017-03-01 09:31:20 +0100

    Mods

---
+++ servlet/src/main/java/fr/iut/HelloServlet.java
@@ -0,0 +1,24 @@
+package fr.iut;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>" );
+		out.println("<HEAD>");
+		out.println("<TITLE>Hello</TITLE>");
+		out.println("</HEAD>");
+		out.println("<BODY>");
+		out.println("<H1>Hello Jorge !</H1>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		out.close();
+	}
+}
